a891d189a9eb178939fb581ef1095c25d3ff7642
Gurveer Parmar <gurveerparmar92@gmail.com>
2021-12-15 11:48:06 +0700

    added json-server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,27 +1,27 @@
-import { createContext, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'this is an example 1',
-      rating: 9,
-    },
-    {
-      id: 2,
-      text: 'this is an example 2',
-      rating: 4,
-    },
-    {
-      id: 3,
-      text: 'this is an example 3',
-      rating: 2,
-    },
-  ]);
+  const [feedback, setFeedback] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    axios.get(`/feedback?_sort=id_order=desc`).then((res) => {
+      setFeedback(res.data);
+      setIsLoading(false);
+    });
+  }, []);
+  /* using fetch
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      `http://localhost:3001/feedback?_sort=id_order=desc`
+    );
+    const data = await response.json;
+    setFeedback(data)
+  };
+*/
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
@@ -37,22 +37,23 @@ export const FeedbackProvider = ({ children }) => {
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to remove item?')) {
+      axios.delete(`/feedback/${id}`);
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
   const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    console.log(newFeedback);
-    setFeedback([newFeedback, ...feedback]);
+    axios.post(`/feedback`, newFeedback).then((res) => {
+      setFeedback(feedback.concat(res.data));
+    });
   };
 
   const updateFeedback = (id, updatedItem) => {
-    setFeedback(
-      feedback.map((item) =>
-        item.id === id ? { ...item, ...updatedItem } : item
-      )
-    );
+    axios.put(`/feedback/${id}`, updatedItem).then((changedItem) => {
+      setFeedback(
+        feedback.map((item) => (item.id !== id ? item : changedItem.data))
+      );
+    });
   };
 
   return (
@@ -60,6 +61,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
